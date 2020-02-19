2870cb7ddeea0190adc278bf0ce3c18693df2a49
dell <jmsugay11@gmail.com>
2020-02-19 20:23:27 +0800

    connection string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,4 +4,4 @@ mongoose.Promise = global.Promise;
 mongoose.connect("mongodb+srv://node_test:santos31193@cluster0-z71wg.mongodb.net/test?retryWrites=true&w=majority" || 'mongodb://localhost:27017/TodoApp');
 
 //mongodb+srv://node_test:santos31193@cluster0-z71wg.mongodb.net/test?retryWrites=true&w=majority
-module.exports = { mongoose };
+module.exports = { mongoose }; 
