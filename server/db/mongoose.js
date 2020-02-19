d143a035f80729179a9abefe9bd8d54272b6ffe6
dell <jmsugay11@gmail.com>
2020-02-20 00:23:04 +0800

    test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,7 @@
 var mongoose =  require('mongoose');
 
 mongoose.Promise = global.Promise;
+mongoose.connect("mongodb+srv://node_test:santos31193@cluster0-z71wg.mongodb.net/test?retryWrites=true&w=majority" || 'mongodb://localhost:27017/TodoApp');
 
-if (location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname === ""){
-    alert("It's a local server!");
-    mongoose.connect('mongodb://localhost:27017/TodoApp');
-}else{
-	mongoose.connect("mongodb+srv://node_test:santos31193@cluster0-z71wg.mongodb.net/test?retryWrites=true&w=majority");
-}
- 
+//mongodb+srv://node_test:santos31193@cluster0-z71wg.mongodb.net/test?retryWrites=true&w=majority
 module.exports = { mongoose }; 
