1a10f8a64b96e725e206a7fab067c76f0b78bff8
dell <jmsugay11@gmail.com>
2020-02-21 01:17:17 +0800

    auth and hashing

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose =  require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://node_test:santos31193@cluster0-z71wg.mongodb.net/test?retryWrites=true&w=majority" || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 //mongodb+srv://node_test:santos31193@cluster0-z71wg.mongodb.net/test?retryWrites=true&w=majority
 module.exports = { mongoose }; 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
+var env = process.env.NODE_ENV || 'development';
+console.log('env ******',env);
+if(env === 'development'){
+	process.env.PORT = 3000;
+	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+}else if(env === 'test'){
+	process.env.PORT = 3000;
+	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
+
 const _ = require('lodash')
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -67,7 +78,6 @@ app.delete('/todos/:id', (req,res) => {
 	});
 });
 
-
 app.patch('/todos/:id',(req,res) => {
 	var id = req.params.id;
 	var body = _.pick(req.body, ['text', 'completed']);
@@ -92,7 +102,28 @@ app.patch('/todos/:id',(req,res) => {
 	}).catch((e) => {
 		response.status(400).send();
 	})
-})
+});
+
+app.post('/users',(req,res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new Users(body);
+ 
+	user.save().then( () => { 
+		return user.generateAuthToken();
+	}).then((token) => {
+		return res.header('x-auth',token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	});
+});
+
+app.get('/users',(req,res) => {
+	Users.find().then((users) => {
+		res.send({users});
+	},(e) => {
+		res.status(400).send(e);
+	})
+});
 
 
 app.listen(port, () => {
