f608300de3ae0a22a492b32a69834f2af5dfcd48
dell <jmsugay11@gmail.com>
2020-02-24 18:46:41 +0800

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data ={
-	id: 10
-}
+var password = '123abc!';
+
+// bcrypt.genSalt(10,(err,salt) => {
+// 	bcrypt.hash(password,salt,(err,hash) =>{
+// 		console.log(hash);
+// 	});
+// })
+
+var hashPasword = "$2a$10$tQ0Kund/B6Bt691/VjTHo.YphgG8sy7UOSuQnEpJbs0cZIjJLrLRa";
+
+bcrypt.compare('123!',hashPasword,(err,res)=>{
+	console.log(res);
+});
+// var data ={
+// 	id: 10
+// }
 
 
-var token = jwt.sign(data,'123abc'); 
-console.log(token);
+// var token = jwt.sign(data,'123abc'); 
+// console.log(token);
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded' , decoded);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded' , decoded);
 // var message ='I am user number 3';
 
 // var hash = SHA256(message).toString();
