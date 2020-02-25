51f4df67ab6ba98f7a4e96c2b690e2bc44419db3
dell <jmsugay11@gmail.com>
2020-02-25 16:46:41 +0800

    database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose =  require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect("mongodb+srv://node_test:santos31193@cluster0-z71wg.mongodb.net/test?retryWrites=true&w=majority");
 
-//mongodb+srv://node_test:santos31193@cluster0-z71wg.mongodb.net/test?retryWrites=true&w=majority
+//
 module.exports = { mongoose }; 
